616a288e3adc82b19bfdfa5c514020a81ba5661f
Thomas <trosselet@gaming.tech>
2026-06-15 16:11:33 +0200

    [FIX] Copy sdl2 dll

--- bin/make.bat
+++ bin/make.bat
@@ -1,2 +1,15 @@
+@echo off 
+
 SolutionGenerator.exe -bp -make
-PAUSE
+
+copy /Y "..\lib\SDL2\lib\x64\*.dll" "..\ide\gcle\"
+
+copy /Y "..\lib\SDL2_ttf\lib\x64\*.dll" "..\ide\gcle\"
+
+copy /Y "..\lib\SDL2_Image\lib\x64\*.dll" "..\ide\gcle\"
+copy /Y "..\lib\SDL2_Image\lib\x64\optional\*.dll" "..\ide\gcle\"
+
+copy /Y "..\lib\SDL2_mixer\lib\x64\*.dll" "..\ide\gcle\"
+copy /Y "..\lib\SDL2_mixer\lib\x64\optional\*.dll" "..\ide\gcle\"
+
+pause
--- src/gcle/main.cpp
+++ src/gcle/main.cpp
@@ -1,5 +1,7 @@
 #include "SDL.h"
 
+#undef main
+
 int main(int argc, char** argv)
 {
     /* Initialisation simple */
