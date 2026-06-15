fed7a58f85b7d5c6241b1e17a06029a291318bc6
Marien-Garon <mgaron@gaming.tech>
2026-06-15 16:49:51 +0200

    [Add] GameManager
    Use sdl renderer and window for now
    Waiting for Class Window

---
+++ src/Engine/GameManager.h
@@ -0,0 +1,31 @@
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+
+class GameManager
+{
+private:
+
+	SDL_Renderer* mp_Renderer = nullptr;
+	SDL_Window* mp_Window = nullptr;
+
+	bool isRunning = false;
+
+
+	int m_WindW, m_WindH;
+
+
+	bool Init();
+	void Loop();
+	void Close();
+
+public:
+
+	GameManager(int windowWidth,int windowHeight) :
+		m_WindW(windowWidth),
+		m_WindH(windowHeight) { }
+
+	void LaunchGame();
+
+};
--- src/gcle/main.cpp
+++ src/gcle/main.cpp
@@ -1,36 +1,41 @@
 #include "SDL.h"
+#include "GameManager.h"
 
 #undef main
 
 int main(int argc, char** argv)
 {
-    /* Initialisation simple */
-    if (SDL_Init(SDL_INIT_VIDEO) != 0)
-    { 
-        return -1;
-    }
+    GameManager gm = GameManager(640,480);
 
-    {
-        /* Création de la fenêtre */
-        SDL_Window* pWindow = NULL;
-        pWindow = SDL_CreateWindow("Ma première application SDL2", SDL_WINDOWPOS_UNDEFINED,
-            SDL_WINDOWPOS_UNDEFINED,
-            640,
-            480,
-            SDL_WINDOW_SHOWN);
+    gm.LaunchGame();
 
-        if (pWindow)
-        {
-            SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voit la fenêtre */
+    ///* Initialisation simple */
+    //if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    //{ 
+    //    return -1;
+    //}
 
-            SDL_DestroyWindow(pWindow);
-        }
-        else
-        { 
-        }
-    }
+    //{
+    //    /* Création de la fenêtre */
+    //    SDL_Window* pWindow = NULL;
+    //    pWindow = SDL_CreateWindow("Ma première application SDL2", SDL_WINDOWPOS_UNDEFINED,
+    //        SDL_WINDOWPOS_UNDEFINED,
+    //        640,
+    //        480,
+    //        SDL_WINDOW_SHOWN);
 
-    SDL_Quit();
+    //    if (pWindow)
+    //    {
+    //        SDL_Delay(3000); /* Attendre trois secondes, que l'utilisateur voit la fenêtre */
+
+    //        SDL_DestroyWindow(pWindow);
+    //    }
+    //    else
+    //    { 
+    //    }
+    //}
+
+    //SDL_Quit();
 
     return 0;
 }
