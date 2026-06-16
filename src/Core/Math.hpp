5ef67be679284f834f6a26a0dc61c59bb2327641
paul-FERRAN <pferran@gaming.tech>
2026-06-16 13:19:11 +0200

    [REFACTO] pi dans math

--- src/Core/Math.hpp
+++ src/Core/Math.hpp
@@ -7,6 +7,8 @@ namespace Math
 	using Radians = float;
 	using Degrees = float;
 
+
+	constexpr float32 PI = 3.14159265358979323846;
 	constexpr float RAD_TO_DEG = 180.0f / PI;
 	constexpr float DEG_TO_RAD = PI / 180.0f;
 
--- src/Core/include.h
+++ src/Core/include.h
@@ -6,5 +6,3 @@
 
 using Vector2u = Vector2<int32>;
 using Vector2f = Vector2<float32>;
-
-constexpr float32 PI = 3.14159265358979323846;
