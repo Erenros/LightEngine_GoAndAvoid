41b608752bdbecbf3f95cf17acb2454fdfa3490c
Simon LUCAS <slucas@gaming.tech>
2026-06-16 09:27:10 +0200

    [FIX] fix problème de surcharge de fonction

--- src/Engine/Window.h
+++ src/Engine/Window.h
@@ -7,6 +7,8 @@ class Window {
 	SDL_Renderer* mp_Renderer = nullptr;
 
 
+public:
+
 	//Constructors
 	Window() = default;
 	Window(const char* pName, int width, int height, Uint32 windowFlags, Uint32 rendererFlags, int x, int y) {
@@ -20,10 +22,8 @@ class Window {
 	};
 
 
-public:
 
 	//Create a Window
-	void Create(const char* pName, int width, int height, Uint32 windowFlags, Uint32 rendererFlags, int x, int y);
 	void Create(const char* pName, int width, int height, Uint32 windowFlags = SDL_WINDOW_SHOWN, Uint32 rendererFlags = SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC, int x = SDL_WINDOWPOS_UNDEFINED, int y = SDL_WINDOWPOS_UNDEFINED);
 	
 	
