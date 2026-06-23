204fcfe32f75574781b07b65afa63f158b2720f6
Simon LUCAS <slucas@gaming.tech>
2026-06-23 16:33:44 +0200

    [ADD] sample scene 2

---
+++ src/gcle/SampleScene2.cpp
@@ -0,0 +1,45 @@
+#include "SampleScene2.h"
+#include "Player.h"
+
+void SampleScene2::OnInitialize()
+{
+	 
+	/*pEntity = CreateEntity<Player>(gcle::Shapes::Rectangle);
+	pEntity->SetTexture(tex);
+	pEntity->SetScale(0.5f);
+	pEntity->SetRigidBody(true);*/
+	 
+	
+
+	Font* font = RessourceManager::GetInstance().GetFont("Hack-Regular");
+
+	std::string text = "Test";
+	CreateText(font,text , 40, 40, 20, 20);
+	 
+	Entity* entity1 = CreateEntity<Entity>(gcle::Shapes::Circle);
+ 	entity1->SetPosition(10, -200);
+	entity1->SetRigidBody(true);
+	entity1->GetShape()->SetIsKinematic(true);
+	 
+	Entity* entity2 = CreateEntity<Entity>(gcle::Shapes::Rectangle);
+	entity2->SetPosition(10, 100);
+	entity2->SetRigidBody(true);
+	entity2->GetRigidBody().SetGravity(false);
+	entity2->GetShape()->SetIsKinematic(false);
+	entity2->SetTexture("images");
+}
+
+void SampleScene2::OnUpdate(Clock& time)
+{
+	//pEntity->Rotate(1);
+
+	if (InputManager::GetInstance().IsDown(Space))
+	{
+		SceneManager::GetInstance().SetCurrentSceneToPreviousScene();
+	}
+
+	if (InputManager::GetInstance().IsDown('A'))
+	{
+		pEntity->Destroy();
+	}
+} 
